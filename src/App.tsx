aad808cb0d30b94b1bee485d48b878d0518bc81d
Gatt101 <gauravpatilk11@gmail.com>
2025-03-27 19:15:20 +0530

    added

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import React from 'react';
+
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
 import { Chat } from './components/Chat';
 import { Footer } from './components/Footer';
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
-  return (
-    <div className="min-h-screen bg-gray-50 dark:bg-gray-900 flex flex-col">
-      <Header />
-      <Hero />
-      <main id="chat-section" className="flex-1 container mx-auto px-4 py-16">
-        <Chat />
-      </main>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className="App">
+            <AnimatedCursor />
+            <div className="min-h-screen bg-gradient-to-b from-blue-50 to-white dark:from-gray-900 dark:to-black flex flex-col">
+                <Header />
+                <Hero />
+                <main id="chat-section" className="flex-1 container mx-auto px-4 py-16">
+                    <Chat />
+                </main>
+                <Footer />
+            </div>
+        </div>
+    );
 }
 
 export default App;
+
