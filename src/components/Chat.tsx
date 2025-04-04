92301c7fda6720b5aa80257984a98d08186dfd70
Gatt101 <gauravpatilk11@gmail.com>
2025-04-04 22:46:18 +0530

    done!!!

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -368,18 +368,18 @@ export const Chat = () => {
       <div className="flex justify-between items-center p-4 border-b border-gray-200 dark:border-gray-700 bg-gradient-to-r from-blue-50 to-indigo-50 dark:from-gray-800 dark:to-gray-750">
         <h2 className="text-xl font-semibold text-gray-900 dark:text-white">Orthopedic Assistant</h2>
         <div className="flex space-x-2">
-          <button
-            onClick={() => handleFindNearbyHospitals("location")}
-            disabled={isLocationLoading}
-            className="p-2 text-gray-500 hover:text-blue-600 dark:text-gray-400 dark:hover:text-blue-400 
-                    rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors disabled:opacity-50"
-            title="Find Nearby Hospitals"
-          >
-            {isLocationLoading ? 
-              <Loader className="h-5 w-5 animate-spin" /> : 
-              <MapPin className="h-5 w-5" />
-            }
-          </button>
+          {/*<button*/}
+          {/*  onClick={() => handleFindNearbyHospitals("location")}*/}
+          {/*  disabled={isLocationLoading}*/}
+          {/*  className="p-2 text-gray-500 hover:text-blue-600 dark:text-gray-400 dark:hover:text-blue-400 */}
+          {/*          rounded-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition-colors disabled:opacity-50"*/}
+          {/*  title="Find Nearby Hospitals"*/}
+          {/*>*/}
+          {/*  {isLocationLoading ? */}
+          {/*    <Loader className="h-5 w-5 animate-spin" /> : */}
+          {/*    <MapPin className="h-5 w-5" />*/}
+          {/*  }*/}
+          {/*</button>*/}
           <button
             onClick={handleReset}
             className="p-2 text-gray-500 hover:text-blue-600 dark:text-gray-400 dark:hover:text-blue-400 
