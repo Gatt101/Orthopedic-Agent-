ede8af9629763200ec35789d1c407b1d32217272
Gatt101 <gauravpatilk11@gmail.com>
2025-06-15 14:20:50 +0530

    done lol

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,1 @@
-export const API_BASE_URL = ['http://127.0.0.1:5000'];
-//'https://bone-fracture-detection-xihx.onrender.com',
+export const API_BASE_URL = ['https://bone-fracture-detection-xihx.onrender.com'];
