5300f15c87bd9bea68bdb48434d995e70ccb4e34
Gatt101 <gauravpatilk11@gmail.com>
2025-07-27 19:28:05 +0530

    added

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = ['https://bonefracturedetection-production.up.railway.app/'];
+export const API_BASE_URL = ['https://bone-fracture-detection-xihx.onrender.com/'];
 //backend fix
