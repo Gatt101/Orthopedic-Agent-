476ab8f6e0bc5503ce3f5f834526c8e4fe85f52e
Gatt101 <gauravpatilk11@gmail.com>
2025-07-27 20:11:35 +0530

    added

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = ['https://bone-fracture-detection-xihx.onrender.com/'];
+export const API_BASE_URL = ['https://bone-fracture-detection-xihx.onrender.com/', 'https://orthopedicagent123-uwwnb.ondigitalocean.app/'];
 //backend fix
